9d5843198988466eb74ad3762a295199437564c1
boudewijn3004 <43668719+boudewijn3004@users.noreply.github.com>
2021-02-11 16:50:39 +0100

    Add files via upload

---
+++ bScripts/CubeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CubeController : MonoBehaviour
+{
+    MidiControls controls;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        controls = new MidiControls();
+        controls.Gameplay.Rotate.performed += ctx => Grow();
+    }
+
+    // Update is called once per frame
+    void Grow()
+    {
+        transform.localScale *= 1.1f;
+       // Console.WriteLine("hij doet het");
+    }
+
+    private void OnEnable()
+    {
+        controls.Gameplay.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Gameplay.Disable();
+    }
+}
